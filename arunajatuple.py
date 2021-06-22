772130d989494b235317dc2db8dac45a8550147e
arunajagp <85545281+arunajagp@users.noreply.github.com>
2021-06-22 19:11:15 +0530

    Add files via upload

---
+++ arunajatuple.py
@@ -0,0 +1,4 @@
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+print(count(mytuple)) #to count the number of elements in a tuple
+print(index("apple")) #to specify the position of the elemnt in the tuple
